c772be47f07b991814dc476f0893ca06d73541b7
Becca King <becca.king3232@gmail.com>
2019-11-21 13:32:52 -0500

    bug fix for allsongs partial

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ const app = angular.module('MyApp', []).config(function($sceProvider){
 
 app.controller('MainController', ['$http', function($http){
 
-  this.includePath = 'partials/viewallsongs.html'
+  this.includePath = 'html/partials/viewallsongs.html'
 
   this.changeInclude = (path) => {
     this.includePath = 'html/partials/' + path + '.html'
@@ -16,9 +16,12 @@ app.controller('MainController', ['$http', function($http){
       url:'/songs'
     }).then(response => {
       this.songs = response.data
+      console.log(this.songs)
     }, error => {
       console.log(error)
     })
   }
+
+this.getSongs()
 
 }]);
