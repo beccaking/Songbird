893e7dcd4384410738aa569bcab8f4e450a73596
Becca King <becca.king3232@gmail.com>
2019-11-21 13:39:32 -0500

    youtube links now working

--- models/songs.js
+++ models/songs.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const songSchema = new mongoose.Schema({
   title: String,
   artist: String,
-  link: String
+  url: String
 })
 
 const Song = mongoose.model('Song', songSchema)
