1f38cf93708ff07e00732ecefb6e51491b491fba
Ayla Ex <aylaex01@gmail.com>
2019-11-21 14:51:08 -0500

    created post route to create session

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const User = require('../models/users.js')
+const Songbird = require('../models/songbirds.js')
+
+router.post('/', (req, res) => {
+    Songbird.findOne({username:req.session.username}, (error, foundUser) => {
+        if(foundUser === null){
+            res.json({
+                message:'user not found'
+            })
+        } else {
+            const doesPasswordMatch = bcrypt.compareSync(req.body.password, foundUser.password);
+            if(doesPasswordMatch){
+                req.session.user = foundUser;
+                res.json(foundUser);
+            } else {
+                res.json({
+                    message:'user not found'
+                })
+            }
+        }
+    })
+})
 
 //log in or set the session cookie
 router.get('/', (req, res) => {
