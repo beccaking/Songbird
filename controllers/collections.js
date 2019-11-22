4a04b7b0ba0432ebec5c8a7ef8fcb4195f8e88a1
Jewls Krueger <jewls@Jewlss-MacBook-Pro.local>
2019-11-22 09:52:30 -0800

    small changes

--- controllers/collections.js
+++ controllers/collections.js
@@ -53,6 +53,7 @@ router.put('/:id', (req, res) => {
 // add a song to a collection
 router.post('/addsong/:id', (req, res) => {
   Collections.findById(req.params.id, (err, foundCollection) => {
+    // console.log(req.body[0]);
     foundCollection.songs.push(req.body[0]);
     foundCollection.save()
     res.json(foundCollection)
